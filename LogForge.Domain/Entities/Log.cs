21dd57630e6fe23b4cd372187878fcf7b07fb97d
Arqam Mousa <arqam.mousa@gmail.com>
2026-08-19 20:05:22 +0300

    Domain: add domain entites.

---
+++ LogForge.Domain/Entities/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace LogForge.Domain.Entities;
+
+public class Log
+{
+    public long Id { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public string Level { get; set; } = null!;
+    public string Service { get; set; } = null!;
+    public string Message { get; set; } = null!;
+    public Dictionary<string, object>? Attributes { get; set; }
+}
---
+++ LogForge.Domain/Entities/LogMinuteRollup.cs
@@ -0,0 +1,9 @@
+﻿namespace LogForge.Domain.Entities;
+
+public class LogMinuteRollup
+{
+    public DateTimeOffset BucketStart { get; set; }
+    public string Service { get; set; } = null!;
+    public string Level { get; set; } = null!;
+    public long LogCount { get; set; }
+}
